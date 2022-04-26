47b5c1aa3e71c6acd32fa5b53fb48fa7e84b33ad
Kodhanda Ramayya Bobbadi <kodhanda.ramayya@kyndryl.com>
2022-04-26 21:49:11 +0530

    8

--- app-deployment.sh
+++ app-deployment.sh
@@ -16,43 +16,43 @@ echo
 echo "Namespace $NAMESPACE"
 
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
     -X GET -o /dev/null -w "%{http_code}")
 
 if [ $status_code == 200 ]; then
   echo
   echo "Updating deployment"
   curl --fail -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/bulletin-board-deployment" \
     -X PATCH -d @bulletin-board-deployment.json
 else
  echo
  echo "Creating deployment"
  curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/deployments" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
     -X POST -d @bulletin-board-deployment.json
 fi
 
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/services/bulletin-board-service" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/api/v1/namespaces/$NAMESPACE/services/bulletin-board-service" \
     -X GET -o /dev/null -w "%{http_code}")
 
 if [ $status_code == 404 ]; then
  echo
  echo "Creating service"
  curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/services" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/api/v1/namespaces/$NAMESPACE/services" \
     -X POST -d @bulletin-board-service.json
 fi
 
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/ingresses/bulletin-board-ingress" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/bulletin-board-ingress" \
     -X GET -o /dev/null -w "%{http_code}")
 
 if [ $status_code == 404 ]; then
  echo
  echo "Creating ingress"
  curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://172.23.93.218:8443/api/v1/namespaces/$NAMESPACE/ingresses" \
+    "https://c5.che01.containers.cloud.ibm.com:21426/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses" \
     -X POST -d @bulletin-board-ingress.json
 fi
